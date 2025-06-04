6295bd5d012dbde0fda568acfb9ddc760cba1702
felixL-K <floyaukahn@gmail.com>
2025-06-04 14:37:15 +0200

    starting to work

--- src/smtml/tmp_log_path.ml
+++ src/smtml/tmp_log_path.ml
@@ -7,10 +7,22 @@ let set path = log_path := Some path
 let get () =
   match !log_path with
   | Some p -> p
-  | None -> Fmt.failwith "Temporary log path not set"
+  | None ->
+    let env_var = "QUERY_LOG_PATH" in
+    match Bos.OS.Env.var env_var with
+    | Some path ->
+        log_path := Some path;
+        path
+    | None ->
+        Fmt.failwith "Temporary log path not set and QUERY_LOG_PATH is not defined"
 
 let init_logging () =
-  let pid = Unix.getpid () in
-  let unique_name = Fmt.str "queries_log_%d.jsonl" pid in
-  let tmp_path = String.concat "" ["/home/intern-fw-03/Documents/queries/"; unique_name] in
-  set tmp_path
+  let env_var = "QUERY_LOG_PATH" in
+  match Bos.OS.Env.var env_var with
+  | Some path ->
+      set path
+  | None ->
+      let pid = Unix.getpid () in
+      let unique_name = Fmt.str "queries_log_%d.jsonl" pid in
+      let tmp_path = String.concat "" ["/home/intern-fw-03/Documents/queries"; unique_name] in
+      set tmp_path
