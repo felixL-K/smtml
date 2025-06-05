302e0f30633000d55b29dc4ed3624f9b2e7af8e8
felixL-K <floyaukahn@gmail.com>
2025-06-05 14:39:28 +0200

    much more clean that way

--- src/smtml/tmp_log_path.ml
+++ src/smtml/tmp_log_path.ml
@@ -1,28 +1,11 @@
 
 
-let log_path : string option ref = ref None
+let log_path : out_channel option ref = ref None
 
-let set path = log_path := Some path
+let set path = log_path := Some (Out_channel.open_gen [ Open_creat; Open_append; Open_text ] 0o644 path)
 
-let get () =
+let write s =
   match !log_path with
-  | Some p -> p
+  | Some p -> Out_channel.output_string p s
   | None ->
-    let env_var = "QUERY_LOG_PATH" in
-    match Bos.OS.Env.var env_var with
-    | Some path ->
-        log_path := Some path;
-        path
-    | None ->
-        Fmt.failwith "Temporary log path not set and QUERY_LOG_PATH is not defined"
-
-let init_logging () =
-  let env_var = "QUERY_LOG_PATH" in
-  match Bos.OS.Env.var env_var with
-  | Some path ->
-      set path
-  | None ->
-      let pid = Unix.getpid () in
-      let unique_name = Fmt.str "queries_log_%d.jsonl" pid in
-      let tmp_path = String.concat "" ["/home/intern-fw-03/Documents/queries"; unique_name] in
-      set tmp_path
+    Fmt.failwith "Temporary log path not set"
